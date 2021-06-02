0f9e25fa6a5b8d80a4e8a7134107b6b0b2db93ab
Sanket Marathe <sanketmarathe80@gmail.com>
2021-06-02 12:10:47 +0530

    Switched to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Dashboard from './components/Dashboard'
 import React from 'react'
+import {
+  HashRouter,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <Dashboard />
+    <HashRouter basename='/'>
+      <Route exact path="/Dashboard/" component={Dashboard}/>
+      <Route exact path="/" component={Dashboard}/>
+      <Route  path="/Dashboard/Charts"component={Dashboard}/>
+      <Route  path="/Dashboard/labTest"component={Dashboard}/>
+      <Route  path="/Dashboard/Map"component={Dashboard}/>
+    </HashRouter>
   );
 }
 
