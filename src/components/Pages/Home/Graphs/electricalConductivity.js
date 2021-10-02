c11b3cf7b1dc545d3ac50864218b0cb7aba46935
ManikantaChavvakula7 <manikanta.chavvakula@outlook.com>
2021-10-02 19:37:19 +0530

    Translation - Minor Fixes

--- src/components/Pages/Home/Graphs/electricalConductivity.js
+++ src/components/Pages/Home/Graphs/electricalConductivity.js
@@ -2,6 +2,7 @@
 import React from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import { Line } from "react-chartjs-2";
+import { useTranslation } from 'react-i18next';
 
 const Graph = (graphData) => {
 
@@ -109,11 +110,12 @@ const Graph = (graphData) => {
 };
 
 const ElectricalConductivity = ({ current, history }) => {
+  const {t} = useTranslation();
   return (
     <>
       <div className="header text-black dark:text-white">
         <h5 className="hidden 2xl:block title text-gray-500 dark:text-gray-400 font-bold">
-        Electrical Conductivity
+        {t('EC')}
         </h5>
         <h5 className="2xl:hidden title text-gray-500 dark:text-gray-400 font-bold">
         EC
