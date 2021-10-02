abc440a56e4128cf68f8a0df2d6900ed4094d807
Sanket Marathe <sanketmarathe80@gmail.com>
2021-10-02 21:26:33 +0530

    Multi-Language feature fix

--- src/components/SelectLanguage.js
+++ src/components/SelectLanguage.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
+import { useTranslation } from 'react-i18next';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -10,15 +11,17 @@ function classNames(...classes) {
 export default function SelectLanguage() {
 
 
-  const [DropdownText, setDropdownText] = useState("Translate");
+  const { t, i18n } = useTranslation();
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
 
-  var changeText = (text) => setDropdownText(text);
 
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
         <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-grey-900">
-          {DropdownText}
+          Translate
           <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
         </Menu.Button>
       </div>
@@ -34,63 +37,21 @@ export default function SelectLanguage() {
       >
         <Menu.Items className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
-            <Menu.Item onClick={() => changeText("Hindi")}>
-              {({ active }) => (
-                <a
-                  href="https://kam--karta--ami-saafwater-com.translate.goog/?_x_tr_sl=en&_x_tr_tl=hi&_x_tr_pto=nui,elem"                  
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Hindi
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="https://kam--karta--ami-saafwater-com.translate.goog/?_x_tr_sl=en&_x_tr_tl=mr&_x_tr_hl=en-GB&_x_tr_pto=nui,se,elem"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Marathi
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="/"
-                  className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
-                  )}
-                >
-                  Coming Soon!
-                </a>
-              )}
-            </Menu.Item>
-            <form method="POST" action="#">
               <Menu.Item>
-                {({ active }) => (
-                  <button
-                    type="submit"
-                    className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'block w-full text-left px-4 py-2 text-sm'
-                    )}
-                  >
-                    Coming Soon!
-                  </button>
-                )}
-              </Menu.Item>
-            </form>
+                  <button className= "btn block px-4 py-2 text-sm" onClick={() => changeLanguage('en')}>English</button>
+               </Menu.Item>
+               <Menu.Item>
+                  <button className= "btn block px-4 py-2 text-sm" onClick={() => changeLanguage('hn')}>Hindi</button>
+               </Menu.Item>
+               <Menu.Item>
+                  <button className= "btn block px-4 py-2 text-sm" onClick={() => changeLanguage('te')}>Telugu</button>
+               </Menu.Item>
+               <Menu.Item>
+                  <button className= "btn block px-4 py-2 text-sm" onClick={() => changeLanguage('kn')}>Kannada</button>
+               </Menu.Item>
           </div>
         </Menu.Items>
       </Transition>
     </Menu>
-  )
+  );
 }
