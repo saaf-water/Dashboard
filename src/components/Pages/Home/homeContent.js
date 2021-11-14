6f1d7d390f2edbeaa729896c5732f58fdff490a9
Hrishikesh24 <rishi2001mahesh@gmail.com>
2021-11-14 09:23:38 +0530

    Finished location Dropdown implementation

--- src/components/Pages/Home/homeContent.js
+++ src/components/Pages/Home/homeContent.js
@@ -1,11 +1,7 @@
 /*homeContent.js integrates all the different components of home page into one.*/
 
-import React, { Fragment, useState, useEffect } from "react";
-import { Listbox, Transition } from '@headlessui/react'
+import React, { useState, useEffect } from "react";
 import useWebSocket from "react-use-websocket";
-import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
-import { LocationMarkerIcon } from '@heroicons/react/outline'
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import TDS from "./Graphs/TDS"
 import ElectricalConductivity from "./Graphs/electricalConductivity"
 import PH from "./Graphs/pH"
@@ -14,6 +10,7 @@ import Turbidity from "./Graphs/Turbidity"
 import Table from "./Table"
 
 import Summary from "./Summary";
+import MenuBar from "./MenuBar";
 //import Heatmap from "./Heatmap/Heatmap";
 
 require('dotenv').config()
@@ -32,10 +29,6 @@ const useThemeDetector = () => {
     }, []);
     return isDarkTheme;
 }
-var nodeLocation = [
-    { id: 20211112, location: 'St. Paul' },
-    { id: 20211113, location: 'Caranzalem' },
-]
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -57,11 +50,12 @@ export default function HomeContent() {
     const historyMax = useWebSocket(socketUrlMax);
     const history = useWebSocket(socketUrl);
     const current = useWebSocket(socketCurrentUrl);
-    const [selected, setSelected] = useState(nodeLocation[0])
+    
 
     useEffect(() => {
         //console.log("Sending Message on Component Mount");
         current.sendMessage("Get Data");
+        
         setTimeout(() => {
             history.sendMessage("Get Data");
         }, 1000);
@@ -89,83 +83,7 @@ export default function HomeContent() {
 
         <>
             <div className="font-roboto flex-col pb-44 space-y-2 container px-5 py-5 mx-auto">
-                <div className="flex flex-row justify-between items-center">
-                    <p className="font-light text-xs md:text-sm truncate">Last Updated: {current.lastJsonMessage
-                        ? current.lastJsonMessage.date + ", " + current.lastJsonMessage.time : (
-                            <SkeletonTheme className="py-1" color="#cfcfcf" highlightColor="#c4c4c4">
-                                <p>
-                                    <Skeleton count={1} />
-                                </p>
-                            </SkeletonTheme>)}</p>
-
-
-                    <Listbox value={selected} onChange={setSelected}>
-                        {({ open }) => (
-                            <>
-                                <Listbox.Label className="hidden sm:block flex-grow text-xs md:text-sm text-right font-medium text-gray-700 px-2">Location</Listbox.Label>
-                                <div className="mt-1 w-36 sm:w-60 relative">
-                                    <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                                        <span className="flex items-center justify-start">
-                                            <LocationMarkerIcon className="text-red-600 flex-shrink-0 h-6 w-6" aria-hidden="true" />
-                                            <span className="ml-2 text-xs sm:text-sm block truncate">{selected.location}</span>
-                                        </span>
-                                        <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                                            <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                                        </span>
-                                    </Listbox.Button>
-
-                                    <Transition
-                                        show={open}
-                                        as={Fragment}
-                                        leave="transition ease-in duration-100"
-                                        leaveFrom="opacity-100"
-                                        leaveTo="opacity-0"
-                                    >
-                                        <Listbox.Options className="absolute z-10 mt-1 right-0 w-60 bg-white shadow-lg max-h-56 rounded-md py-1 text-xs ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm">
-                                            {nodeLocation.map((area) => (
-                                                <Listbox.Option
-                                                    key={area.id}
-                                                    className={({ active }) =>
-                                                        classNames(
-                                                            active ? 'text-white bg-indigo-600' : 'text-gray-900',
-                                                            'cursor-default select-none relative py-2 pl-3 pr-9'
-                                                        )
-                                                    }
-                                                    value={area}
-                                                >
-                                                    {({ selected, active }) => (
-                                                        <>
-                                                            <div className="flex items-center">
-                                                                <LocationMarkerIcon className={classNames(selected ? 'text-gray-900' : 'text-gray-400', "flex-shrink-0 h-6 w-6")} />
-                                                                <span
-                                                                    className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block')}
-                                                                >
-                                                                    {area.location}
-                                                                </span>
-                                                            </div>
-
-                                                            {selected ? (
-                                                                <span
-                                                                    className={classNames(
-                                                                        active ? 'text-white' : 'text-indigo-600',
-                                                                        'absolute inset-y-0 right-0 flex items-center pr-4'
-                                                                    )}
-                                                                >
-                                                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                                </span>
-                                                            ) : null}
-                                                        </>
-                                                    )}
-                                                </Listbox.Option>
-                                            ))}
-                                        </Listbox.Options>
-                                    </Transition>
-                                </div>
-                            </>
-                        )}
-                    </Listbox>
-                </div>
-
+                <MenuBar current={current} />
                 <div className="flex flex-wrap -m-4 ">
                     <div className="p-4 w-full lg:w-2/4 xl:w-3/5">
                         <div className="flex flex-row">
